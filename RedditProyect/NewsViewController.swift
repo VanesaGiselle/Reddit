97022bceeeb30db3f5d8c24fdcb1caa87f1daa32
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2023-04-07 11:50:13 -0300

    Configure the proyect with programmatic view.

---
+++ RedditProyect/NewsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsViewController.swift
+//  RedditProyect
+//
+//  Created by Vanesa Korbenfeld on 07/04/2023.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
+
